318c93b756974998d9b086cb6ece2e6a69e8a1a8
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2021-10-04 14:28:26 -0700

    created LoginForm and SignupForm models in Authentication.

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
