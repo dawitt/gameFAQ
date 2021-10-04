821126910ffabc67aecdc068c3d05752194a263c
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2021-10-04 14:38:24 -0700

    created templates folder and included the index.html file, and also the generic_form.html. Also created the signup_view, login_view, and logout_view in the authentication views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from authentication.forms import LoginForm, SignupForm
+from django.shortcuts import redirect, render, reverse
+from users.models import MyUser
+
 
 # Create your views here.
+def signup_view(request):
+    template_name = "generic_form.html"
+    form = SignupForm()
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = MyUser.objects.create_user(
+                username=data.get("username"), password=data.get("password")
+            )
+            login(request, user)
+            return redirect(reverse("homepage"))
+    return render(request, template_name, {"form": form, "header": "Signup"})
+
+
+def login_view(request):
+    template_name = "generic_form.html"
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data.get("username"), password=data.get("password")
+            )
+            if user:
+                login(request, user)
+                return redirect(request.GET.get("next", "/"))
+    return render(request, template_name, {"form": form, "header": "Login"})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("homepage"))
