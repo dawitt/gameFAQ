e3ef87d24083d658aac04338090d304667edce49
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2021-10-04 14:30:09 -0700

    Oops! I moved the LoginForm and SignupForm to forms.py. Nothing should be in models. I placed it in the wrong file.

---
+++ authentication/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
