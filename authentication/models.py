3452b9c971617049f3987898c7f4e09fba1f5336
BethanyFolino <bethany.folino@gmail.com>
2021-10-04 16:14:34 -0700

    Fixed secret key

--- authentication/models.py
+++ authentication/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
 # Create your models here.
-from django import forms
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
-class SignupForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
