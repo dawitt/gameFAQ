c1e03c4eb28e5f670fbd1a83b90c924f10e099a9
Efgon <egonzalezgar@gmail.com>
2021-10-05 13:59:21 -0400

    added to the users app the Comment Post, Question Post and FAQpost models and registered the to the admin site and the form for each. and also made the Secret Keyvisible again in settings

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CommentPosts, QuestionPosts, FAQPosts
 # Register your models here.
+admin.site.register(CommentPosts)
+admin.site.register(QuestionPosts)
+admin.site.register(FAQPosts)
