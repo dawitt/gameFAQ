29873c40b988f4a9db06cea3261b68b791d68525
Efgon <egonzalezgar@gmail.com>
2021-10-05 14:27:01 -0400

    Added notification model and registered it to site

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.db.models.fields import NullBooleanField
+from users.models import MyUser
+from posts.models import CommentPosts, QuestionPosts, FAQPosts
 # Create your models here.
+class Notifications(models.Model):
+    
+    mentioned = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='mentioned')
+    comment_post = models.ForeignKey(CommentPosts, on_delete=models.CASCADE, related_name='comment_post', blank=True, null=True)
+    question_post = models.ForeignKey(QuestionPosts, on_delete=models.CASCADE, related_name='question_post', blank=True, null=True)
+    faq_post = models.ForeignKey(FAQPosts, on_delete=models.CASCADE, related_name='faq_post', blank=True, null=True)
+    seen = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.post
