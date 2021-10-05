68ef5c04bdb11dfe44378b324f9ec50d2a1ce367
Efgon <egonzalezgar@gmail.com>
2021-10-05 16:46:07 -0400

    This is the beginig of creating views. created homepage_view, user detail view,  comments,questions, faqs detail views.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django import forms
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from posts.forms import AddQuestionPost
+from users.models import MyUser
+from posts.models import CommentPosts, QuestionPosts, FAQPosts
+from notification.models import Notifications
+from django.views.generic import View 
 # Create your views here.
+
+def homepage_view(request):
+    user = request.user
+    form = AddQuestionPost()
+    return render(request, 'index.html', {'user':user})
+
+
+class Question_detail_view(View):
+    def get(self, request, id):
+        question_post = QuestionPosts.objects.filter(id=id)
+        return render(request, 'generic_post_detail.html', {'question_post':question_post})
+
+
+
+class Comment_detail_view(View):
+    def get(self, request, id):
+        comment_post = CommentPosts.objects.filter(id=id)
+        return render(request, 'generic_post_detail.html', {'question_post':comment_post})
+
+class Faq_detail_view(View):
+    def get(self, request, id):
+        faq_post = FAQPosts.objects.filter(id=id)
+        return render(request, 'generic_post_detail.html', {'question_post':faq_post})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from users.models import MyUser
+from posts.models import QuestionPosts, CommentPosts, FAQPosts
+from django.views.generic import View
 # Create your views here.
+
+class User_detail_view(View):
+    def get(self, request, id):
+        user = MyUser.objects.get(id=id)
+        questions = QuestionPosts.objects.filter(question_creator=user)
+        comments = CommentPosts.objects.filter(creator=user)
+        faqs = FAQPosts.objects.filter(faq_creator=user)
+        return render(request, 'user_detail.html', {'user': user, 'questions':questions, 'comments': comments, 'faqs':faqs})
+        
+
