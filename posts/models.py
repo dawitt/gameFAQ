af37a1093e106520c1fd9c3f9d3c8403792e3f36
BethanyFolino <bethany.folino@gmail.com>
2021-10-09 16:23:46 -0700

    Changed faq to walkthrough, FaqPost is now WalkthroughPost, WalkthroughPost is now WalkthroughComment, added notification view just to have a third class based view, made walkthrough detail view a regular view, changed console detail view so the tags etc will not show on page, attempted like and dislike for answers and walkthroughs but redirect breaks even though it otherwise works

--- posts/models.py
+++ posts/models.py
@@ -7,46 +7,45 @@ from PIL import Image
 class QuestionPost(models.Model):
     question_title = models.CharField(max_length=200)
     question_body = models.CharField(max_length=250)
-    question_img = models.ImageField(upload_to='uploaded_imgages/', null=True, blank=True)
+    question_img = models.ImageField(upload_to='uploaded_images/', null=True, blank=True)
     question_creator = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
     for_game = models.CharField(max_length=100)
     
-
     def __str__(self):
         return self.question_title
 
 class AnswerPost(models.Model):
-    answer_body = models.TextField(max_length=200)
-    answer_img = models.ImageField(upload_to='uploaded_imgages/', null=True, blank=True)
+    answer_body = models.TextField()
+    answer_img = models.ImageField(upload_to='uploaded_images/', null=True, blank=True)
     question = models.ForeignKey(QuestionPost, on_delete=models.CASCADE)
     answer_creator = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
-    #likes = models.IntegerField(default=0)
-    #dislikes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
     def __str__(self):
         return self.answer_body
 
 
-class FAQPost(models.Model):
-    faq_title = models.CharField(max_length=100)
-    faq_body = models.TextField(max_length=250)
-    faq_img = models.ImageField(upload_to='uploaded_imgages/', null=True, blank=True)
-    faq_creator = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+class WalkthroughPost(models.Model):
+    walkthrough_title = models.CharField(max_length=100)
+    walkthrough_body = models.TextField()
+    walkthrough_img = models.ImageField(upload_to='uploaded_images/', null=True, blank=True)
+    walkthrough_creator = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
     for_game = models.CharField(max_length=100)
-    #likes = models.IntegerField(default=0)
-    #dislikes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
     def __str__(self):
-        return self.faq_title
+        return self.walkthrough_title
 
-class WalkthroughPost(models.Model):
-    post_body = models.CharField(max_length=250)
-    faq_post = models.ForeignKey(FAQPost, on_delete=models.CASCADE)
-    post_img = models.ImageField(upload_to='uploaded_imgages/', null=True, blank=True)
+class WalkthroughComment(models.Model):
+    post_body = models.TextField()
+    walkthrough_post = models.ForeignKey(WalkthroughPost, on_delete=models.CASCADE)
+    post_img = models.ImageField(upload_to='uploaded_images/', null=True, blank=True)
     creator = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
     for_game = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.post_title
+        return self.post_body
--- users/views.py
+++ users/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from users.models import MyUser
-from posts.models import QuestionPost, WalkthroughPost, FAQPost
+from posts.models import QuestionPost, WalkthroughPost, WalkthroughComment
 from django.views.generic import View
 # Create your views here.
 
-class User_detail_view(View):
+class UserDetailView(View):
     def get(self, request, id):
         user = MyUser.objects.get(id=id)
         questions = QuestionPost.objects.filter(question_creator=user)
-        walkthrough_comments = WalkthroughPost.objects.filter(creator=user)
-        faqs = FAQPost.objects.filter(faq_creator=user)
-        return render(request, 'user_detail.html', {'user': user, 'questions':questions, 'walkthrough_comments': walkthrough_comments, 'faqs':faqs})
+        walkthroughs = WalkthroughPost.objects.filter(walkthrough_creator=user)
+        return render(request, 'user_detail.html', {'user': user, 'questions':questions, 'walkthroughs': walkthroughs})
         
 
