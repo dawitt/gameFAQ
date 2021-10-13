d04f40a1585e48edd956f0f8b97e9ed22a894252
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2021-10-13 09:44:41 -0700

    created 404 and 500 html template pages, added 2 views for it in the POST views.py page. Need to add the URL path next

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,8 @@ from notification.models import Notification
 from django.views.generic import View 
 import requests
 import json
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 # Create your views here.
 
 class HomepageView(View):
@@ -191,3 +193,18 @@ def add_walkthrough(request, id):
     return render(request, 'generic_form.html', {'form':form})
 
 
+#Got404/500 info from: 
+# https://stackoverflow.com/questions/17662928/django-creating-a-custom-500-404-error-page
+
+def handler404(request, *args, **argv):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
