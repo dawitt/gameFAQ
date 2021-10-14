d7fd15cce2b20e3767629832766f5be2ce695de8
dawitt <delfan2003@gmail.com>
2021-10-13 22:07:55 -0400

    Made the picture thumbnail in search and all console games and trying to put profile picture on the navbar

--- users/models.py
+++ users/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class MyUser(AbstractUser):
     display_name = models.CharField(max_length=30)
+    profile_img = models.ImageField(upload_to='images/', null=True, blank=True )
