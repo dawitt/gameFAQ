fed7e7aa94427e39c7cbfbcfa821ab242a0333ed
Efgon <egonzalezgar@gmail.com>
2021-10-15 14:53:31 -0400

    added more content to the readme file

--- users/models.py
+++ users/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+# from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class MyUser(AbstractUser):
     display_name = models.CharField(max_length=30)
+<<<<<<< Updated upstream
     profile_img = models.ImageField(upload_to='images/', null=True, blank=True )
 
+=======
+    profile_img = models.ImageField(upload_to='imgages/', null=True, blank=True)
+    favorite_games = models.CharField(max_length=100, null=True, blank=True)
+>>>>>>> Stashed changes
