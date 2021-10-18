b87340494336ad63c81695d132af07f6cca65988
Efgon <egonzalezgar@gmail.com>
2021-10-18 15:18:48 -0400

    removed img icon from navbar

--- users/models.py
+++ users/models.py
@@ -5,10 +5,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class MyUser(AbstractUser):
     display_name = models.CharField(max_length=30)
-<<<<<<< Updated upstream
-    profile_img = models.ImageField(upload_to='images/', null=True, blank=True )
-
-=======
     profile_img = models.ImageField(upload_to='imgages/', null=True, blank=True)
-    favorite_games = models.CharField(max_length=100, null=True, blank=True)
->>>>>>> Stashed changes
+    
